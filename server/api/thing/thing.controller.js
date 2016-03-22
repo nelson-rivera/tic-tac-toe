7bbb69f7e30dca6e7297a33d01d5c7e91c32a760
Nelson Rivera <nelson.rivera@domandtom.com>
2016-03-22 16:56:10 -0600

    Leaving API for a while

--- server/api/thing/thing.controller.js
+++ server/api/thing/thing.controller.js
@@ -15,6 +15,7 @@ import {Thing} from '../../sqldb';
 function respondWithResult(res, statusCode) {
   statusCode = statusCode || 200;
   return function(entity) {
+    console.log(res);
     if (entity) {
       res.status(statusCode).json(entity);
     }
@@ -56,6 +57,40 @@ function handleError(res, statusCode) {
   return function(err) {
     res.status(statusCode).send(err);
   };
+}
+
+export function isWinner(req, res) {
+  const board = req.body.board;
+  const play = req.body.play;
+  const dimension = board.length;
+  const code = board[play.x][play.y];
+
+  let rowSum = 0, colSum = 0, diagLSum = 0, diagRSum = 0;
+  let leftX= 0 , rightX = 0;
+
+  for(let i=0; i < dimension; i++){
+    if(board[play.x][i] === code) {
+      rowSum++;
+    }
+    if(board[i][play.y] === code) {
+      colSum++;
+    }
+    leftX = i + (play.x - play.y);
+    if(leftX >= 0 && leftX < dimension){
+      if(board[leftX][i] === code){
+        diagLSum++;
+      }
+    }
+    rightX = play.x + play.y - i;
+    if(rightX >= 0 && rightX < dimension){
+      if(board[rightX][i] === code){
+        diagRSum++;
+      }
+    }
+  }
+  //Return if one of the results is equal to the needed for win the game
+  const isWinner = colSum >= dimension || rowSum >= dimension || diagLSum >= dimension || diagRSum  >= dimension;
+  return respondWithResult({'isWinner' : isWinner});
 }
 
 // Gets a list of Things
