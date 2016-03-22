524bc60b0b838b5121f42020df3ad4a618f50c15
Nelson Rivera <nelson.rivera@domandtom.com>
2016-03-22 12:44:29 -0600

    Initial commit, project generated with yeoman

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('ticTacToeApp')
+  .directive('navbar', () => ({
+    templateUrl: 'components/navbar/navbar.html',
+    restrict: 'E',
+    controller: 'NavbarController',
+    controllerAs: 'nav'
+  }));
