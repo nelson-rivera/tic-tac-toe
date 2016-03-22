e3afda35371d3d59ae1acef0f98137619becb818
Nelson Rivera <nelson.rivera@domandtom.com>
2016-03-22 15:07:13 -0600

    Labeling cells

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -8,13 +8,25 @@ class MainController {
     this.$http = $http;
     this.dimension = 3;
     this.board = [];
+    this.players = [
+      {
+        code: 'x',
+        name: 'Player X'
+      },
+      {
+        code: '0',
+        name: 'Player 0'
+      }
+    ];
+    this.playerInTurn;
+    this.movements = 0;
   }
 
   $onInit() {
     // this.$http.get('/api/things').then(response => {
     //   this.awesomeThings = response.data;
     // });
-
+    this.playerInTurn = 0;
     this.board = this.createBoard(this.dimension, null);
   }
 
@@ -28,6 +40,20 @@ class MainController {
     }
     return board;
   }
+
+  play = function(x,y) {
+    console.log(x);
+    console.log(y);
+
+    const play = {
+      'x': x,
+      'y': y
+    }
+    if(this.board[x][y] === null){
+      this.board[x][y] = this.players[this.playerInTurn].code;
+    }
+  }
+
 }
 
 angular.module('ticTacToeApp')
